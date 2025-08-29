955247fc1c08c4c5e926c3705b515606c3fa595c
alshifa2307 <alshifa2307@gmail.com>
2025-08-29 13:44:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* contact us section */
+function formSubmit(a){
+    var c=document.getElementById('name').value;
+    alert('form is submiited successfully '+c+" !!");
+}
+
+/* sign in section */
+function signSubmit()
+{
+    var d=document.getElementById('username').value;
+    alert('you have signed in successfully');
+}
+function signUpSubmit()
+{
+    var d=document.getElementById('username').value;
+    alert('you have signed up successfully');
+}
+
+//offer section
+function applied()
+{
+    alert('your offer has been successfully applied to your order!! 🤗');
+}
